e2b06fa285c917c432d1053f699e9587177a4447
timnew <timnew.wti@gmail.com>
2012-04-23 14:13:37 +0800

    Added Ability for self-installation

--- ProvisionDaemon/Program.cs
+++ ProvisionDaemon/Program.cs
@@ -11,14 +11,94 @@ namespace ProvisionDaemon
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static int Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-			{ 
-				new ProvisionDaemon() 
-			};
-            ServiceBase.Run(ServicesToRun);
+            if (args.Length > 0)
+            {
+                try
+                {
+                    switch (args[0].ToLowerInvariant())
+                    {
+                        case "install":
+                            return Install();
+                        case "uninstall":
+                            return Uninstall();
+                        case "run":
+                        case "launch":
+                            return LaunchService();
+                    }
+
+                }
+                catch (Exception)
+                {
+                    return -1;
+                }
+            }
+
+            return LaunchService();
+        }
+
+        private static int LaunchService()
+        {
+            try
+            {
+                var ServicesToRun = new ServiceBase[] 
+			    { 
+				    new ProvisionDaemon() 
+			    };
+
+                ServiceBase.Run(ServicesToRun);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return -1;
+            }
+            return 0;
+        }
+
+        private static int Uninstall()
+        {
+            var stateSaver = new System.Collections.Hashtable();
+
+            var installer = new ProvisionDaemonInstaller();
+
+            try
+            {
+                installer.Uninstall(stateSaver);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                installer.Rollback(stateSaver);
+
+                return -1;
+            }
+
+            installer.Commit(stateSaver);
+            return 0;
+        }
+
+        private static int Install()
+        {
+            var stateSaver = new System.Collections.Hashtable();
+
+            var installer = new ProvisionDaemonInstaller();
+
+            try
+            {
+                installer.Install(stateSaver);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                installer.Rollback(stateSaver);
+                return -1;
+            }
+
+            installer.Commit(stateSaver);
+            return 0;
         }
     }
 }
