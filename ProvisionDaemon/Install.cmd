46278545a31ff750bbc8696130fb0b281299cf84
timnew <timnew.wti@gmail.com>
2012-04-23 17:18:30 +0800

    Prepare for Build Script

--- ProvisionDaemon/Install.cmd
+++ ProvisionDaemon/Install.cmd
@@ -1,2 +1,2 @@
-﻿C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe ProvisionDaemon.exe
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe ProvisionDaemon.exe
 pause
---
+++ Tools/Build.ps1
@@ -0,0 +1,5 @@
+$solutionDir = $1
+$outputDir = $2
+$7z = Join-Path $solutionDir "Tools\7z\7z.exe"
+
+echo $solutionDir, $outputDir, $7z
